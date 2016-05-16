3cfaccb4190fd74aa7977412ea78e6aa8a619a29
xiaolei <Coder_xiaolei@163.com>
2016-05-16 18:21:25 +0800

    combineLatest,reduce,filter,ignoreSignal

---
+++ RACTest/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  RACTest
+//
+//  Created by Madis on 16/5/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
--- RACTest/ViewController.m
+++ RACTest/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "TableViewController.h"
 static NSString * const cellIdentifier = @"cell";
 
 @interface ViewController ()
@@ -23,7 +23,7 @@ UITableViewDataSource>
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    _cellNameArray = @[@"concat",@"zipWith",@"then",@"merge",@"combineLatest",@"reduce"];
+    _cellNameArray = @[@"concat",@"zipWith",@"then",@"merge",@"combineLatest",@"reduce",@"filter",@"ignoreSignal"];
     
     [self createView];
 }
@@ -58,6 +58,8 @@ UITableViewDataSource>
         case 3: [self mergeSignal]; break;
         case 4: [self combineLatestSignal]; break;
         case 5: [self reduceSignal]; break;
+        case 6: [self filterSignal]; break;
+        case 7: [self ignoreSignal]; break;
 
         default:break;
     }
@@ -144,12 +146,14 @@ UITableViewDataSource>
 {
     RACSignal *signalA = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
         [subscriber sendNext:@1];
+        MSLog(@"李磊----signalA");
         [subscriber sendCompleted];
         return  nil;
     }];
     
     RACSignal *signalB = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
         [subscriber sendNext:@2];
+        MSLog(@"李磊----signalB");
         [subscriber sendCompleted];
         return nil;
     }];
@@ -184,11 +188,13 @@ UITableViewDataSource>
         return nil;
     }];
     RACSignal *signalB = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
-        [subscriber sendNext:@2];
+//        [subscriber sendNext:@2];
         MSLog(@"李磊----signalB");
         [subscriber sendCompleted];
         return nil;
     }];
+    //只要signal中有任何一个改变就会触发subscribeNext操作
+    //且只要有signal,sendNext就会有输出,适用于登录注册等动态操作
     [[signalA merge:signalB] subscribeNext:^(id x) {
         MSLog(@"李磊----%@",x);
     } error:^(NSError *error) {
@@ -197,22 +203,135 @@ UITableViewDataSource>
         MSLog(@"李磊----完成");
     }];
 }
-
+/*!
+ *  @author madis, 16-05-16 16:05:06
+ *
+ *  将多个信号合并起来，并且拿到各个信号的最新的值,必须每个合并的signal至少都有过一次sendNext，才会触发合并的信号。
+ */
+// 底层实现：
+// 1.当组合信号被订阅，内部会自动订阅signalA，signalB,必须两个信号都发出内容，才会被触发。
+// 2.并且把两个信号组合成元组发出。
 - (void)combineLatestSignal
 {
+    RACSignal *signalA = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@1];
+        MSLog(@"李磊----signalA");
+        [subscriber sendCompleted];
+        return nil;
+    }];
+    RACSignal *signalB = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+//        [subscriber sendNext:@2];
+        MSLog(@"李磊----signalB");
+        [subscriber sendCompleted];
+        return nil;
+    }];
+    //某个signal,sendNext不成功时表示newSignal没combineLatestWith成功,不会subscribeNext消息
+    RACSignal *newSignal = [signalA combineLatestWith:signalB];
+    [newSignal subscribeNext:^(id x) {
+        MSLog(@"李磊----%@",x);
+    } error:^(NSError *error) {
+        MSLog(@"李磊----%@",error.localizedDescription);
+    } completed:^{
+        MSLog(@"李磊----完成");
+    }];
     
+//    [[RACSignal
+//      combineLatest:@[self.priceInput.rac_textSignal,
+//                      self.nameInput.rac_textSignal,
+//                      RACObserve(self, isConnected)
+//                      ]
+//      reduce:^(NSString *price, NSString *name, NSNumber *connect){
+//          return @(price.length > 0 && name.length > 0 && ![connect boolValue]);
+//      }]
+//     subscribeNext:^(NSNumber *res){
+//         if ([res boolValue]) {
+//             NSLog(@"XXXXX send request");
+//         }
+//     }];
 }
-
+/*!
+ *  @author madis, 16-05-16 16:05:05
+ *
+ *  聚合:用于信号发出的内容是元组，把信号发出元组的值聚合成一个值
+ */
+// 底层实现:
+// 1.订阅聚合信号，每次有内容发出，就会执行reduceblcok，把信号内容转换成reduceblcok返回的值。
 - (void)reduceSignal
 {
-    
+    RACSignal *signalA = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@1];
+        MSLog(@"李磊----signalA");
+        [subscriber sendCompleted];
+        return nil;
+    }];
+    RACSignal *reduceSignal = [RACSignal combineLatest:@[signalA,
+                               [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@2];
+        MSLog(@"李磊----signalB");
+        [subscriber sendCompleted];
+        return nil;
+    }]] reduce:^id(NSNumber *num1,NSNumber *num2){
+        //reduce返回两个signal的sendNext值
+        //适用于登陆注册,将usename和password一起合成一个signal传出
+        MSLog(@"李磊----signalReduce");
+        return [NSString stringWithFormat:@"%@,%@",num1,num2];
+    }];
+    [reduceSignal subscribeNext:^(id x) {
+        MSLog(@"李磊-----%@",x);
+    } error:^(NSError *error) {
+        MSLog(@"李磊----%@",error.localizedDescription);
+    } completed:^{
+        MSLog(@"李磊----完成");
+    }];
 }
-- (void)combineLatestReduceSignal
+/*!
+ *  @author madis, 16-05-16 17:05:01
+ *
+ *  过滤信号，使用它可以获取满足条件的信号.
+ */
+// 每次信号发出，会先执行过滤条件判断.
+- (void)filterSignal
 {
-//    RACSignal *reduceSignal = [RACSignal combineLatest:@[signalA,signalB] reduce:^id(NSNumber *num1 ,NSNumber *num2){
-
+    UITextField *textField = [[UITextField alloc]init];
+    [[textField.rac_textSignal filter:^BOOL(id value) {
+        return textField.text.length >10;
+    }] subscribeNext:^(id x) {
+        MSLog(@"李磊-----%@",x);
+    } error:^(NSError *error) {
+        MSLog(@"李磊----%@",error.localizedDescription);
+    } completed:^{
+        MSLog(@"李磊----完成");
+    }];
+    
+    //KVC观察input值的变化
+//    [[RACObserve(self, input)
+//      filter:^(NSString* value){
+//          if ([value hasPrefix:@"2"]) {
+//              return YES;
+//          } else {
+//              return NO;
+//          }
+//      }] subscribeNext:^(NSString* x){
+//          request(x);//发送一个请求
+//      }];
 }
-
+/*!
+ *  @author madis, 16-05-16 18:05:59
+ *
+ *  ignore:忽略完某些值的信号.
+ */
+// 内部调用filter过滤，忽略掉ignore的值
+- (void)ignoreSignal
+{
+    UITextField *textField = [[UITextField alloc]init];
+    [[textField.rac_textSignal ignore:@"1"] subscribeNext:^(id x) {
+        MSLog(@"李磊---%@",x);
+    }];
+    
+//    [[RACObserve(self, input) ignore:@"1"] subscribeNext:^(id x) {
+//        MSLog(@"李磊---%@",x);
+//    }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
